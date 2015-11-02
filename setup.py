3d2f03f5c4a7e235f14511fc995bd34a57ad7439
Mikhail <glader.ru@gmail.com>
2015-11-02 09:29:54 +0500

    Fix install process

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 #-*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import YaDiskClient
+import codecs
+import os
+import re
 
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+    
 setup(
     name='YaDiskClient',
-    version=YaDiskClient.__version__,
+    version=find_version('YaDiskClient', '__init__.py'),
     include_package_data=True,
     py_modules=['YaDiskClient'],
     url='https://github.com/TyVik/YaDiskClient',
